9c491cc595215b97fd57f05a62bb62f552dd2d8c
j43hyun9 <j43hyun9@gmail.com>
2025-01-22 02:51:44 +0900

    5-2,5-3 비슷함 코드는 5-3

--- 5_1/5_1.ino
+++ 5_1/5_1.ino
@@ -3,7 +3,7 @@ void setup() {
   for ( int i = 2; i <= 9; i++) {
     pinMode(i, OUTPUT);
   }
-  digitalWrite(2, HIGH); // 시작과 동시에 LED 요소 a에 불이 켜짐
+  digitalWrite(9, HIGH); // 시작과 동시에 LED 요소 a에 불이 켜짐
 
 }
 
